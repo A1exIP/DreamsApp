c2875faa40773e1b46800e13ea96787a804c73f6
ippol <ippolitov16@gmail.com>
2024-04-13 22:35:56 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/dreamsapp/MainActivity.java
@@ -0,0 +1,161 @@
+  package com.example.dreamsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+  public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    private ArrayList<String> taskList;
+    private ArrayAdapter<String> adapter;
+    private EditText editTextTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent((MainActivity.this,Text_Content_Activity.class));
+//                startActivity(intent);
+//            }
+//        });
+
+        // Analysis
+        Button analysis_button = (Button) findViewById(R.id.button_analysis); // Находим кнопку в макете по ее идентификатору
+        analysis_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View activity_analysis) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(MainActivity.this, AnalysisActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+        // Устанавливаем обработчик нажатия кнопки
+//        analysis_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View activity_analysis) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_analysis);
+//            }
+//        });
+        // Calendar
+        Button calendar_button = findViewById(R.id.button_calendar); // Находим кнопку в макете по ее идентификатору
+        calendar_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button_calendar) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        calendar_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_calendar) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_calendar);
+//            }
+//        });
+        // Target
+        Button target_button = findViewById(R.id.button_target); // Находим кнопку в макете по ее идентификатору
+        target_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button_target) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(MainActivity.this, TargetActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        target_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_target) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_target);
+//            }
+//        });
+        // Account
+        Button account_button = findViewById(R.id.button_account); // Находим кнопку в макете по ее идентификатору
+        account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button_account) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        account_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_account) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_account);
+//            }
+//        });
+
+
+        // Инициализация списков задач
+        taskList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
+
+        // Привязка элементов пользовательского интерфейса
+        Button addButton = findViewById(R.id.button_menu);
+        ListView taskListView = findViewById(R.id.listViewTasks);
+//        TextView editTextTask = findViewById(R.id.textView);
+        editTextTask = findViewById(R.id.editTextTextPersonName);
+
+        // Назначение адаптера для списка задач
+        taskListView.setAdapter(adapter);
+
+        // Обработчик нажатия на кнопку добавления задачи
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View add_new) {
+                addTask();
+            }
+        });
+
+        // Обработчик нажатия на элемент списка задач для удаления
+        taskListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                removeTask(position);
+            }
+        });
+    }
+
+    // Метод для добавления задачи
+    private void addTask() {
+        String task = editTextTask.getText().toString().trim();
+        if (!task.isEmpty()) {
+            taskList.add(task);
+            adapter.notifyDataSetChanged();
+            editTextTask.getText().clear();
+        } else {
+            Toast.makeText(this, "Введите задачу", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Метод для удаления задачи
+    private void removeTask(int position) {
+        taskList.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+}
---
+++ app/src/main/java/com/example/dreamsapp/TargetActivity.java
@@ -0,0 +1,152 @@
+  package com.example.dreamsapp;
+
+  import android.content.Intent;
+  import android.os.Bundle;
+  import android.view.View;
+  import android.widget.AdapterView;
+  import android.widget.ArrayAdapter;
+  import android.widget.Button;
+  import android.widget.EditText;
+  import android.widget.ListView;
+  import android.widget.Toast;
+
+  import androidx.appcompat.app.AppCompatActivity;
+
+  import java.util.ArrayList;
+
+  public class TargetActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    private ArrayList<String> taskList;
+    private ArrayAdapter<String> adapter;
+    private EditText editTextTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target);
+
+        // Analysis
+        Button analysis_button = findViewById(R.id.button_analysis); // Находим кнопку в макете по ее идентификатору
+        analysis_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(TargetActivity.this, AnalysisActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        analysis_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View activity_analysis) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_analysis);
+//            }
+//        });
+        // Calendar
+        Button calendar_button = findViewById(R.id.button_calendar); // Находим кнопку в макете по ее идентификатору
+        calendar_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(TargetActivity.this, CalendarActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        calendar_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_calendar) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_calendar);
+//            }
+//        });
+        // Target
+        Button target_button = findViewById(R.id.button_target); // Находим кнопку в макете по ее идентификатору
+        target_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(TargetActivity.this, TargetActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        target_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_target) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_target);
+//            }
+//        });
+        // Account
+        Button account_button = findViewById(R.id.button_account); // Находим кнопку в макете по ее идентификатору
+        account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Устанавливаем обработчик нажатия кнопки
+                // Создаем новое намерение для перехода на другую активность
+                Intent intent = new Intent(TargetActivity.this, AccountActivity.class);
+                startActivity(intent); // Запускаем новую активность
+            }
+        });
+//        account_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View button_account) {
+//                // Загружаем другой макет
+//                setContentView(R.layout.activity_account);
+//            }
+//        });
+
+
+        // Инициализация списков задач
+        taskList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
+
+        // Привязка элементов пользовательского интерфейса
+        Button addButton = findViewById(R.id.button_menu);
+        ListView taskListView = findViewById(R.id.listViewTasks);
+//        TextView editTextTask = findViewById(R.id.textView);
+        editTextTask = findViewById(R.id.editTextTextPersonName);
+
+        // Назначение адаптера для списка задач
+        taskListView.setAdapter(adapter);
+
+        // Обработчик нажатия на кнопку добавления задачи
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+
+        // Обработчик нажатия на элемент списка задач для удаления
+        taskListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                removeTask(position);
+            }
+        });
+    }
+
+    // Метод для добавления задачи
+    private void addTask() {
+        String task = editTextTask.getText().toString().trim();
+        if (!task.isEmpty()) {
+            taskList.add(task);
+            adapter.notifyDataSetChanged();
+            editTextTask.getText().clear();
+        } else {
+            Toast.makeText(this, "Введите задачу", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Метод для удаления задачи
+    private void removeTask(int position) {
+        taskList.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+}
